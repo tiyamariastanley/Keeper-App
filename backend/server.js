919ebcca4295e76de58126552d57efc7f33bfa9d
Tiya <tiyamaria96@gmail.com>
2021-02-06 00:14:59 +0530

    Deploying web-app in heroku

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require('cors');
 const mongoose = require("mongoose");
 const router = require('./routes/index');
+const path = require("path");
 
 const app = express();
 
@@ -10,19 +12,19 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "client", "build")))
 app.use("/", router);
 
-mongoose.connect("mongodb+srv://tiyamaria:timast96@cluster0.l99fr.mongodb.net/keeperDB",{ useNewUrlParser: true ,useUnifiedTopology: true,useFindAndModify: false});
+mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true ,useUnifiedTopology: true,useFindAndModify: false});
 mongoose.connection.once("open", function() {
   console.log("Connection with MongoDB was successful");
 });
 
-//app.use("/", router);
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 5000;
-}
+let port = process.env.PORT||5000;
 app.listen(port, function() {
   console.log("Server started on port " +port);
 });
